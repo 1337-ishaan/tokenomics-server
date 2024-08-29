2d9f4ab2a2b4925aa47f2babad2919643b4f6f03
Ishaan Parmar <64636222+1337-ishaan@users.noreply.github.com>
2024-08-29 18:31:51 +0530

    fix: env

--- src/lib/anthropic.ts
+++ src/lib/anthropic.ts
@@ -1,5 +1,7 @@
+/// <reference types="node" />
+
 import Anthropic from "@anthropic-ai/sdk";
 
 export const anthropic = new Anthropic({
-    apiKey: "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"
+    apiKey: process.env.ANTHROPIC_API_KEY  
   });
